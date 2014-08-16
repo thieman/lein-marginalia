4274acf8594103289cb26d726d0d7afce551c52a
Travis Thieman <travis.thieman@gmail.com>
2014-08-16 19:25:03 -0400

    Use thieman marginalia fork

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject lein-marginalia "0.8.0-SNAPSHOT"
+(defproject thieman/lein-marginalia "0.8.0-SNAPSHOT"
   :description "Leiningen plugin for Marginalia."
-  :url "http://github.com/fogus/marginalia"
-  :dependencies [[marginalia "0.8.0-SNAPSHOT"]]
+  :url "http://github.com/thieman/marginalia"
+  :dependencies [[thieman/marginalia "0.8.0-SNAPSHOT"]]
   :eval-in :leiningen)
-
